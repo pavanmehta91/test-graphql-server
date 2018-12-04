831297c1b119294c534164f63fa5cd27ce528869
Pavan <pavanmehta91@bitbucket.com>
2018-12-05 00:59:17 +0530

    adding more queries and mutations

--- fake-data.js
+++ fake-data.js
@@ -1,42 +1,232 @@
+const users = [
+  {
+    id: 1,
+    name: "Pavan"
+  },
+  {
+    id: 2,
+    name: "Dhaval"
+  },
+  {
+    id: 3,
+    name: "Khushboo"
+  },
+  {
+    id: 4,
+    name: "Nishant"
+  },
+  {
+    id: 5,
+    name: "Nihar"
+  },
+  {
+    id: 6,
+    name: "Jigar"
+  },
+  {
+    id: 7,
+    name: "Aakash"
+  }
+];
+const questions = [
+  {
+    id: 1,
+    upvoteCount: 0,
+    title: "Which is the best mobile phone company?",
+    body:
+      "Can anbody explain to me in detail, with all features and prons and cons. I want to select the best phone under the budget 20k",
+    tags: [
+      {
+        name: "mobile"
+      },
+      {
+        name: "cell phones"
+      }
+    ],
+    by: {
+      id: 2
+    }
+  },
+  {
+    id: 2,
+    title: "How to improve internet speed?",
+    upvoteCount: 0,
+    body: "What ways could be applied to maximize the broadband speed?",
+    tags: [
+      {
+        name: "internet"
+      }
+    ],
+    by: {
+      id: 1
+    }
+  },
+  {
+    id: 3,
+    title: "Why is the color of sky blue?",
+    upvoteCount: 0,
+    body: "Is there a reason behind sky's color being blue?",
+    tags: [
+      {
+        name: "info"
+      },
+      {
+        name: "general knowledge"
+      }
+    ],
+    by: {
+      id: 4
+    }
+  },
+  {
+    id: 4,
+    title: "What is the best tool to test graphql resolvers",
+    upvoteCount: 0,
+    body:
+      "Please suggest best GUI, CLI or web based tools to test the graphql endpoints",
+    tags: [
+      {
+        name: "graphql"
+      },
+      {
+        name: "schema"
+      },
+      {
+        name: "testing"
+      }
+    ],
+    by: {
+      id: 5
+    }
+  }
+];
+
+const answers = [
+  {
+    upvoteCount: 0,
+    id: 1,
+    text:
+      "Moto One Power is a great option. You can also go for MI A2. Both come with stock android",
+    by: {
+      id: 1
+    },
+    on: {
+      id: 1
+    }
+  }
+];
 module.exports = {
- users : [],
- questions : [],
- answers : [],
- upvotes : [],
- favorites: [],
+  users,
+  questions: questions,
+  answers: answers,
+  upvotes: [],
+  favorites: []
+};
+/*
+QUERIES AND MUTATIONS:
+# Write your query or mutation here
+mutation addUser($name:String!){
+  user: addUser(name: $name){
+    id
+    name
+  }
 }
-/*
-users: {
-id:
-name:
-email
-},
-questions: {
-id:
-title:
-body:
-by:
-upvoteCount:
-tags: [
-  name:
-]
-},
-answers: {
-id:
-question_id:
-upvoteCount:
-text:
-by:
-},
 
-*/
+mutation addQuestion($title:String!, $body:String!, $tags: [Tags!]!){
+  addQuestion(title:$title, body:$body, tags: $tags){
+    id
+  }
+}
 
-/*
-type Upvote {
-  id: Int!
-  type: UpvoteType
-  on: Int!
-  value: UpvoteValue
-  by: User!
+query getQuestions($tag:String, $userId: Int){
+  questions: getQuestions(tag: $tag, userId: $userId){
+    id
+    isUpvoted
+    isFavorited
+    body
+    tags
+    title
+    by{
+      name
+      id
+    }
+  }
+}
+
+mutation addAnswer($text:String!, $on:Int!){
+  answer: addAnswer(text:$text, on: $on){
+    id
+    text
+    by{
+      id
+    }
+    on{
+      id
+    }
+    upvoteCount
+  }
+}
+query getQuestion($id: Int!){
+  question:getQuestion(id: $id){
+    id
+    isUpvoted
+    isFavorited
+    body
+    tags
+    title
+    by{
+      name
+      id
+    }
+    answers{
+      id
+      text
+      by {
+        id
+        name
+      }
+      isUpvoted
+    }
+  }
+}
+
+mutation upvote($type: UpvoteType, $value: UpvoteValue, $on: Int!){
+  upvote(type:$type, value: $value, on: $on)
+}
+mutation favoriteAction($on: Int!){
+  favorited: favoriteAction(on: $on)
+}
+
+mutation editQuestion($title: String, $id: Int!){
+  editQuestion(title:$title, id: $id){
+    id
+    title
+    body
+    tags
+  }
+}
+query getAnswers{
+  myAnswers: getAnswers{
+    id
+    text
+    upvoteCount
+    on{
+      id
+    }
+  }
+}
+
+query getUser($id: Int!){
+  user: getUser(id: $id){
+    id
+    name
+  }
+}
+query favoritedQuestions{
+  questions: favoritedQuestions{
+    id
+    title
+    body
+  }
 }
 */
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, ForbiddenError, AuthenticationError, UserInputError } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
@@ -22,11 +22,84 @@ const resolvers = {
   JSON: GraphQLJSON,
   Query: {
     getQuestions: async (parent, args, context, info) => {
-      return questions;
+      const { user } = context;
+      let loggedInUserID = user ? user.id : "";
+      const { userId, tag } = args;
+      let questionsResponse = [...questions];
+      if (userId) {
+        questionsResponse = questionsResponse.filter(q => q.by.id === userId); //getMyQuestions
+      }
+      if (tag) {
+        questionsResponse = questionsResponse.filter(
+          q =>
+            q.tags.some(t => {
+              return t.name.toLowerCase() === tag.toLowerCase();
+            }) //filteredbyTags
+        );
+      }
+      return questionsResponse.map(q => {
+        q = { ...q };
+        q.isFavorited = favorites.some(
+          f => q.id === f.on && f.by === loggedInUserID
+        );
+        q.isUpvoted = upvotes.some(
+          u => q.id === u.on && u.type === "QUESTION" && u.by === loggedInUserID
+        );
+        return q;
+      });
+    },
+    getQuestion: (parent, args, context, info) => {
+      const { id } = args;
+      const { user } = context;
+      let loggedInUserID = user ? user.id : "";
+      let question = questions.find(q => q.id === id);
+      if (question) {
+        question = { ...question };
+        question.by = users.find(u => u.id === question.by.id);
+        question.isFavorited = favorites.some(
+          f => id === f.on && f.by === loggedInUserID
+        );
+        question.isUpvoted = upvotes.some(
+          u => id === u.on && u.type === "QUESTION" && u.by === loggedInUserID
+        );
+        question.answers = answers
+          .filter(a => a.on.id === id)
+          .map(a => {
+            a = { ...a };
+            a.by = users.find(u=> u.id === a.by.id);
+            a.isUpvoted = upvotes.some(
+              u =>
+                a.id === u.on && u.type === "ANSWER" && u.by === loggedInUserID
+            );
+            return a;
+          });
+        return question;
+      }
+      return null;
     },
     getUser: async (parent, args, context, info) => {
       const { id } = args;
       return users.find(u => u.id === id);
+    },
+    getAnswers: (parent, args, context, info) => {
+      const { user } = context;
+      if (!user) {
+        return []
+      }
+      const {id: by } = user;
+      return answers.filter(a => a.by.id = by);
+    },
+    favoritedQuestions: (parent, args, context, info) => {
+      const { user } = context;
+      if (!user) {
+       throw new AuthenticationError('You must be logged in');
+      }
+      const { id: by } = user;
+      let favQuestions = favorites.filter(f => by === f.by);
+      return favQuestions.map(favQ=> {
+        return questions.find(q=> q.id === favQ.on);
+      });
+
     }
   },
   Mutation: {
@@ -35,37 +108,102 @@ const resolvers = {
       users.push(user);
       return user;
     },
+    editQuestion: (parent, args, context, info) => {
+      const { user } = context;
+      if (!user) {
+        throw new AuthenticationError('You must be logged in')
+      }
+      const { id: by } = user;
+      const { id, ...rest } = args;
+      let question = questions.find(q => q.id === id);
+      if (question && question.by.id === by) {
+        question = { ...question, ...rest };
+        const index = questions.findIndex(q => q.id === id);
+        questions[index] = question;
+        return question;
+      } else {
+        throw new ForbiddenError(`Operation Not Allowed`);
+      }
+    },
     addQuestion: (parent, args, context, info) => {
-      const by = { id: 1 }; //userId
-      const question = { ...args, id: questions.length + 1, by };
+      const { user } = context;
+      console.log({ user });
+      if (!user) {
+        throw new AuthenticationError('You must be logged in')
+      }
+      //const { id: by } = user;
+      console.log("adding question");
+      const question = {
+        ...args,
+        id: questions.length + 1,
+        by: user,
+        upvoteCount: 0
+      };
       questions.push(question);
       return question;
     },
     addAnswer: (parent, args, context, info) => {
-      const by = { id: 1 }; //userId
-      const answer = { ...args, id: answers.length + 1, by };
+      const { user } = context;
+      if (!user) {
+        throw new AuthenticationError('You must be logged in')
+      }
+      const { id: by } = user;
+      const answer = {
+        ...args,
+        id: answers.length + 1,
+        by: user,
+        upvoteCount: 0
+      };
+      answer.on = questions.find(q => q.id === answer.on);
       answers.push(answer);
       return answer;
     },
     upvote: (parent, args, context, info) => {
-      const by = { id: 1 }; //userId
+      const { user } = context;
+      let shouldUpdateUpVoteCounter = true;
+      if (!user) {
+        throw new AuthenticationError('You must be logged in');
+      }
+      const { id: by } = user;
       const { on, value, type } = args;
+      const upvoteOn = type === "ANSWER" ? answers : questions;
+      const toUpvoteObj = upvoteOn.find(a => a.id === on && a.by !== by);
+      if (!toUpvoteObj || toUpvoteObj.by.id === by ) {
+        throw new ForbiddenError(`Operation Not Allowed`);
+      }
       const upvoteObj = upvotes.find(
-        u => u.on === on && u.by === by.id && upvote.type === type
+        u => u.on === on && u.by.id === by && u.type === type
       );
       if (upvoteObj) {
-        upvoteObj.value += value;
+        shouldUpdateUpVoteCounter = upvoteObj.value === value ? false: true;
+        upvoteObj.value = value;
       } else {
         const upvote = { ...args, id: upvotes.length + 1, by };
         upvotes.push(upvote);
       }
-      const upvoteOn = type === "ANSWER" ? answers : questions;
-      const toUpvoteObj = upvoteOn.find(a => a.id === u.on);
-      if (toUpvoteObj) toUpvoteObj.upvoteCount += value;
-      return toUpvoteObj ? toUpvoteObj.upvoteCount : -1; // throw error otherwise toUpvoteObj not found
+
+      if(shouldUpdateUpVoteCounter) toUpvoteObj.upvoteCount += value;
+      return toUpvoteObj.upvoteCount;
     },
     favoriteAction: (parent, args, context, info) => {
       const { on } = args;
+      const { user } = context;
+      if (!user) {
+        throw new AuthenticationError('You must be logged in')
+      }
+      const { id: by } = user;
+      let favObj = favorites.find(f => f.on === on && context.user.id === f.by);
+      if (favObj) {
+        favObj.isFavorited = !favObj.isFavorited;
+      } else {
+        favObj = {
+          by,
+          isFavorited: true,
+          on
+        };
+        favorites.push(favObj);
+      }
+      return favObj.isFavorited;
     }
   }
 };
@@ -75,12 +213,12 @@ const typeDefs = fs.readFileSync(
   "utf8"
 );
 const server = new ApolloServer({
-	cors: true,
-	typeDefs,
+  cors: true,
+  typeDefs,
   resolvers,
   context: ({ req }) => {
-		const userId = req.headers.userId || "";
-    const user = users.find(u => u.id === userId);
+    const userId = req.headers.userid || -1;
+    const user = users.find(u => u.id === parseInt(userId, 10));
     return {
       user
     };
